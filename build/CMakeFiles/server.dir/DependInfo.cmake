3796804dc414a57e4c28ca342ea272700ffbdfca
Sapir Tobias <sapirtobias1@gmail.com>
2026-07-12 17:20:14 +0300

    pulled

---
+++ build/CMakeFiles/server.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/TLP/OneDrive/Talpiot/Year1/SemesterB/Uni/Communication_Networks_Workshop/homework/ex1/tcp_server.cpp" "CMakeFiles/server.dir/tcp_server.cpp.o" "gcc" "CMakeFiles/server.dir/tcp_server.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
